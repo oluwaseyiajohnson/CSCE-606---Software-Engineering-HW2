0742531248bc2fb2b5e6894545d8b6310717c829
Oluwaseyi Johnson <oluwaseyiajohnson@gmail.com>
2025-09-23 21:18:36 -0500

    added new seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,15 +18,15 @@
 #   end
 more_movies = [
 
-  {:title => 'My Neighbor Totoro', :rating => 'G', :release_date => '16-Apr-1988'},
+  # {:title => 'My Neighbor Totoro', :rating => 'G', :release_date => '16-Apr-1988'},
 
-  {:title => 'Green Book', :rating => 'PG-13', :release_date => '16-Nov-2018'},
+  # {:title => 'Green Book', :rating => 'PG-13', :release_date => '16-Nov-2018'},
 
-  {:title => 'Parasite', :rating => 'R', :release_date => '30-May-2019'},
+  # {:title => 'Parasite', :rating => 'R', :release_date => '30-May-2019'},
 
-  {:title => 'Nomadland', :rating => 'R', :release_date => '19-Feb-2021'},
+  # {:title => 'Nomadland', :rating => 'R', :release_date => '19-Feb-2021'},
 
-  {:title => 'CODA', :rating => 'PG-13', :release_date => '13-Aug-2021'}
+  # {:title => 'CODA', :rating => 'PG-13', :release_date => '13-Aug-2021'}
 
   {:title => 'Robocop', :rating=>'R', :release_date=>'17-Jul-1987'}
 
