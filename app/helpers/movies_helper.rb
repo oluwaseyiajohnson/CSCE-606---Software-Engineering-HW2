175d716834997a46b8956e9b2e303f076789e288
Oluwaseyi Johnson <oluwaseyiajohnson@gmail.com>
2025-09-24 23:27:57 -0500

    fixed sort functionality

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,18 +2,17 @@ module MoviesHelper
 
   def sort_column(title, column_key, preference)
     
-    is_sorted = (preference&.last_column == column_key)
-    direction = preference&.direction || "asc"
-    
-    
-    arrow = ""
-    if is_sorted
-      arrow = direction == "asc" ? " ↑" : " ↓"
-    end
-    
-    
-    css_class = is_sorted ? "main" : ""
-    link = link_to "#{title}#{arrow}", movies_path(sort: column_key)
-    content_tag :th, link, class: css_class
+  is_sorted = (preference&.last_column == column_key)
+  direction = preference&.direction || "asc"
+  
+  new_direction = is_sorted && direction == "asc" ? "desc" : "asc"
+
+  arrow = is_sorted ? (direction == "asc" ? " ↓" : " ↑") : ""
+  css_class = is_sorted ? "main" : ""
+
+  link = link_to "#{title}#{arrow}", movies_path(sort: column_key, direction: new_direction)
+  content_tag :th, link, class: css_class
   end
 end
+
+
